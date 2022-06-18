70d2dcb6fb18096efb7a94afc2276026b966d6c1
Galyminsky-PC-Win <jobforandroid@protonmail.com>
2022-06-18 09:33:50 +0300

    Реализая экранов

--- app/src/main/java/com/jobforandroid/shopapp/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/jobforandroid/shopapp/presentation/ShopItemActivity.kt
@@ -1,19 +1,46 @@
 package com.jobforandroid.shopapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.jobforandroid.shopapp.R
 
 class ShopItemActivity : AppCompatActivity() {
-
-    private lateinit var viewModel: ShopItemViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
 
-        viewModel.errorInputName.value = false
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        Log.d("MyLog", mode.toString())
+
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_EDIT = "mod_edit"
+        private const val MODE_ADD = "mod_add"
+
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
+            return intent
+        }
 
 
     }
+
+
 }
+
